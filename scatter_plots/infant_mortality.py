145d4ee3fbd357206428d6c7b9bbd4adb61c9c53
Behnam Javanmardi <behjava@gmail.com>
2023-02-03 16:24:36 +0100

    Update infant_mortality.py

--- scatter_plots/infant_mortality.py
+++ scatter_plots/infant_mortality.py
@@ -1,5 +1,5 @@
 # By B. Javanmardi (behnamjavanmardi.com)
-# Visualising Estimated infant mortality per 1000 live births in Europe 
+# Visualising Estimated infant mortality per 1000 live births in Europe
 # data file HFA_73_EN.csv obtained from European Health Information Gateway.
 # Years data is available in this file: 1950—2019
 # read file skip metadata rows
@@ -17,4 +17,17 @@ EURO_mean=data.groupby(['YEAR']).mean()
 fig=px.scatter(EURO_mean, size='value', color='value', color_continuous_scale=[[0, 'green'], [0.5, 'yellow'], [1, 'red']],
                title="Estimated infant mortality per 1000 live births")
 fig.update_yaxes(title_text='Deaths per 1000 live births')
+
+fig.update(layout_coloraxis_showscale=False)
+
+fig.add_annotation(dict(font=dict(color='black',size=15)), x=1955, y=30,
+            text="©vizmeister.com",
+            showarrow=False,
+            yshift=1)
+
+fig.add_annotation(dict(font=dict(color='black',size=15)), x=2000, y=50,
+            text="Data Source: European Health Information Gateway (WHO)",
+            showarrow=False,
+            yshift=1)
+
 fig.show()
