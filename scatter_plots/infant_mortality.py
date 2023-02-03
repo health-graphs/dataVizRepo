c1ebf3a747b52d41441b2d00cf46dac8c50602d9
Behnam Javanmardi <behjava@gmail.com>
2023-02-03 15:53:51 +0100

    Update infant_mortality.py

--- scatter_plots/infant_mortality.py
+++ scatter_plots/infant_mortality.py
@@ -1,17 +1,20 @@
 # By B. Javanmardi (behnamjavanmardi.com)
+# Visualising Estimated infant mortality per 1000 live births in Europe 
 # data file HFA_73_EN.csv obtained from European Health Information Gateway.
 # Years data is available in this file: 1950—2019
 # read file skip metadata rows
 
-import numpy as np
 import pandas as pd
 import plotly.express as px
 
-data=pd.read_csv('/Users/javanmardi/Work/DataViz/dataViz_data/infant_mortality/HFA_73_EN.csv', sep=',', skiprows=25, skipfooter=56, header=0)
+data=pd.read_csv('/Users/javanmardi/Work/DataViz/dataViz_data/infant_mortality/HFA_73_EN.csv', sep=',',
+                    skiprows=25, skipfooter=56, header=0)
 
 # group by year and average
-mean_EUR_infant_mortality=data.groupby(['YEAR']).mean()
+EURO_mean=data.groupby(['YEAR']).mean()
 
-# scatter plot
-fig = px.scatter(mean_EUR_infant_mortality)
+# scatter plot using customized color_continuous_scale
+fig=px.scatter(EURO_mean, size='value', color='value', color_continuous_scale=[[0, 'green'], [0.5, 'yellow'], [1, 'red']],
+               title="Estimated infant mortality per 1000 live births")
+fig.update_yaxes(title_text='Deaths per 1000 live births')
 fig.show()
