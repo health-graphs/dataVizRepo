915f641c162c930a0c463390e7984fd0dd6f03fa
Behnam Javanmardi <behjava@gmail.com>
2023-02-04 12:14:32 +0100

    Update infant_mortality.py

--- scatter_plots/infant_mortality_Europe/infant_mortality.py
+++ scatter_plots/infant_mortality_Europe/infant_mortality.py
@@ -4,8 +4,15 @@
 # Years data is available in this file: 1950—2019
 # read file skip metadata rows
 
+import os
 import pandas as pd
 import plotly.express as px
+
+
+def assure_path_exists(raw_path):
+        dir = os.path.dirname(raw_path)
+        if not os.path.exists(dir):
+                os.makedirs(dir)
 
 data=pd.read_csv('data/HFA_73_EN.csv', sep=',',
                     skiprows=25, skipfooter=56, header=0)
@@ -15,7 +22,7 @@ EURO_mean=round(data.groupby(['YEAR']).mean(),1)
 
 # scatter plot using customized color_continuous_scale
 fig=px.scatter(EURO_mean, size='value', color='value', color_continuous_scale=[[0, 'green'], [0.5, 'yellow'], [1, 'red']],
-               title="Infant mortality in Europe per 1000 live births")
+               title="Infant mortality in Europe per 1000 live births since 1950")
 fig.update_yaxes(title_text='Deaths per 1000 live births')
 fig.update(layout_coloraxis_showscale=False)
 fig.add_annotation(dict(font=dict(color='#3366ff',size=15)), x=2000, y=46,
@@ -27,5 +34,8 @@ fig.add_annotation(dict(font=dict(color='grey',size=15)), x=2000, y=43,
             text="Data Source: European Health Information Gateway (WHO)",
             showarrow=False,
             yshift=1)
+
+assure_path_exists('output/')
+fig.write_html("output/out.html")
 
 fig.show()
