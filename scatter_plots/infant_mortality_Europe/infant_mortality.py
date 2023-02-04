ae45f2e802c7b1bf74eb00633e0e22495a0d1c76
Behnam Javanmardi <behjava@gmail.com>
2023-02-04 11:27:15 +0100

    Update infant_mortality.py

--- scatter_plots/infant_mortality_Europe/infant_mortality.py
+++ scatter_plots/infant_mortality_Europe/infant_mortality.py
@@ -1,4 +1,4 @@
-# By B. Javanmardi (behnamjavanmardi.com)
+# By B. Javanmardi (https://github.com/behjava)
 # Visualising Estimated infant mortality per 1000 live births in Europe
 # data file HFA_73_EN.csv obtained from European Health Information Gateway.
 # Years data is available in this file: 1950—2019
@@ -7,7 +7,7 @@
 import pandas as pd
 import plotly.express as px
 
-data=pd.read_csv('/Users/javanmardi/Work/DataViz/dataViz_data/infant_mortality/HFA_73_EN.csv', sep=',',
+data=pd.read_csv('data/HFA_73_EN.csv', sep=',',
                     skiprows=25, skipfooter=56, header=0)
 
 # group by year and average
@@ -18,12 +18,12 @@ fig=px.scatter(EURO_mean, size='value', color='value', color_continuous_scale=[[
                title="Infant mortality in Europe per 1000 live births")
 fig.update_yaxes(title_text='Deaths per 1000 live births')
 fig.update(layout_coloraxis_showscale=False)
-fig.add_annotation(dict(font=dict(color='black',size=15)), x=1955, y=30,
-            text="©DataVization.com",
+fig.add_annotation(dict(font=dict(color='#3366ff',size=15)), x=2000, y=46,
+            text="Visualised by DataVization.com",
             showarrow=False,
             yshift=1)
 
-fig.add_annotation(dict(font=dict(color='black',size=15)), x=2000, y=50,
+fig.add_annotation(dict(font=dict(color='grey',size=15)), x=2000, y=43,
             text="Data Source: European Health Information Gateway (WHO)",
             showarrow=False,
             yshift=1)
