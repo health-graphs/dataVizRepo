0ef2a870c88c936293820dfc49da41367d7898ef
Behnam Javanmardi <behjava@gmail.com>
2023-02-13 22:59:03 +0100

    Update cancer_incidence.py

--- scatter_plots/incidence_of_cancer_Europe/cancer_incidence.py
+++ scatter_plots/incidence_of_cancer_Europe/cancer_incidence.py
@@ -1,5 +1,7 @@
 # By B. Javanmardi (https://github.com/behjava)
-# Visualising
+# Visualising Incidence of cancer per 100 000 in Europe
+# data file HFA_357_EN.csv obtained from European Health Information Gateway.
+# some outliers were rejected before ploting. The data source should be consulted for details.
 
 import os
 import pandas as pd
@@ -18,9 +20,14 @@ data=pd.read_csv('data/HFA_357_EN.csv', sep=',',
 # group by year and average
 EURO_mean=round(data.groupby(['YEAR']).mean(),1)
 
+
+# filtered due to incomplete or outlier behaviour of the data points. The data tabel and the source should be consulted for more details.
+filtered=EURO_mean.loc[1980:2018]
+
 # scatter plot using customized color_continuous_scale
-fig=px.scatter(EURO_mean, size='value', color='value', color_continuous_scale=[[0, 'green'], [0.5, 'yellow'], [1, 'red']],
+fig=px.scatter(filtered, size='value', color='value', color_continuous_scale=[[0, 'green'], [0.5, 'yellow'], [1, 'red']],
                title="Average ...")
+
 
 
 
