c52ffa7ad96011dfa990bc3a43fac89d03999be8
Behnam Javanmardi <behjava@gmail.com>
2023-12-14 16:23:07 +0100

    Update cancer_statsitics_US.py
    remove sizes and edit copyright

--- pie_charts/cancer_statistics_US/cancer_statistics_US.py
+++ pie_charts/cancer_statistics_US/cancer_statistics_US.py
@@ -20,33 +20,33 @@ data=pd.read_csv('data/US_Cancer_statistics_2023.csv', sep=';', header=0)
 
 
 # for males
-fig = px.pie(data, values='Estimated new cases Male', names='Cancer Type', width=900, height=900, title='Estimated 2023 cancer statistics for males in the United States')
+fig = px.pie(data, values='Estimated new cases Male', names='Cancer Type', title='Estimated 2023 cancer statistics for males in the United States')
 
-fig.add_annotation(dict(font=dict(color='#3366ff',size=15)), x=0.9, y=1,
-            text="Visualised by DataDeed.de",
+fig.add_annotation(dict(font=dict(color='black',size=10)), x=0.01, y=0.01,
+            text="©DataDeed.de",
             showarrow=False,
             yshift=1)
-
-fig.add_annotation(dict(font=dict(color='grey',size=15)), x=0.95, y=0.97,
-            text="Data from Siegel et al. (2023)",
-            showarrow=False,
-            yshift=1)
+#
+# fig.add_annotation(dict(font=dict(color='grey',size=15)), x=0.95, y=0.97,
+#             text="Data from Siegel et al. (2023)",
+#             showarrow=False,
+#             yshift=1)
 
 fig.update_traces(textposition='inside')
 fig.update_layout(uniformtext_minsize=12, uniformtext_mode='hide')
 
 # for females
-fig2 = px.pie(data, values='Estimated new cases Female', names='Cancer Type', width=900, height=900, title='Estimated 2023 cancer statistics for females in the United States')
+fig2 = px.pie(data, values='Estimated new cases Female', names='Cancer Type', title='Estimated 2023 cancer statistics for females in the United States')
 
-fig2.add_annotation(dict(font=dict(color='#3366ff',size=15)), x=0.9, y=1,
-            text="Visualised by DataDeed.de",
+fig2.add_annotation(dict(font=dict(color='black',size=10)), x=0.01, y=0.01,
+            text="©DataDeed.de",
             showarrow=False,
             yshift=1)
-
-fig2.add_annotation(dict(font=dict(color='grey',size=15)), x=0.95, y=0.97,
-            text="Data from Siegel et al. (2023)",
-            showarrow=False,
-            yshift=1)
+#
+# fig2.add_annotation(dict(font=dict(color='grey',size=15)), x=0.95, y=0.97,
+#             text="Data from Siegel et al. (2023)",
+#             showarrow=False,
+#             yshift=1)
 
 fig2.update_traces(textposition='inside')
 fig2.update_layout(uniformtext_minsize=12, uniformtext_mode='hide')
